323aa6993abbe1fdee49e427f58b9529b62f8b67
JeremyBourg <jeremy.bourg@outlook.com>
2023-11-13 12:00:14 -0500

    cours du 13 nov

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    string _monNom = "Jérémy";
+    bool _vraiNom = true;
+    int _age = 29;
+    bool _verite = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("bruh");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/OuverturePortes.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OuverturePortes : MonoBehaviour
+{
+
+    [SerializeField] GameObject _porteDroite;
+    [SerializeField] GameObject _porteGauche;
+    [SerializeField] GameObject _joueur;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(_porteDroite);
+        Debug.Log(_porteGauche);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player"){
+            _porteDroite.GetComponent<Animator>().SetBool("Ouvre",true);
+            _porteGauche.GetComponent<Animator>().SetBool("Ouvre",true);
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player"){
+            _porteDroite.GetComponent<Animator>().SetBool("Ouvre",false);
+            _porteGauche.GetComponent<Animator>().SetBool("Ouvre",false);
+        }
+    }
+}
